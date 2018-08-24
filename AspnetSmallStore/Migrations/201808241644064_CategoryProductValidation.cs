fd5f70c239e99152aeba92a0c598d2023726a3cf
ilgulee <iglee2000ca@gmail.com>
2018-08-24 12:50:42 -0400

    Adding Data validation and formatiiong constraints to Model class.

---
+++ AspnetSmallStore/Migrations/201808241644064_CategoryProductValidation.cs
@@ -0,0 +1,22 @@
+namespace AspnetSmallStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CategoryProductValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "Name", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Products", "Description", c => c.String(nullable: false, maxLength: 200));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Description", c => c.String());
+            AlterColumn("dbo.Products", "Name", c => c.String());
+            AlterColumn("dbo.Categories", "Name", c => c.String());
+        }
+    }
+}
